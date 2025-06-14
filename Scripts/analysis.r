0ff7868d34c1fc84dbd4eeac804531aaf5e76d03
Hevar <hevar.ness7@gmail.com>
2025-06-14 11:00:54 +0300

    fixed the hard coded gene

--- Scripts/analysis.r
+++ Scripts/analysis.r
@@ -39,7 +39,7 @@ merged_data <- merged_data %>% select(-sample_id)
 head(merged_data) # now the data is much cleaner
 
 # time for some data visualization
-ggplot(data = merged_data, aes(x = condition, y = expression, fill = condition)) + # nolint
+boxplot_of_condition <- ggplot(data = merged_data, aes(x = condition, y = expression, fill = condition)) + # nolint
   geom_boxplot(alpha = 0.5, size = 1.5) +
   geom_jitter(aes(color = age)) +
   labs(
@@ -48,6 +48,8 @@ ggplot(data = merged_data, aes(x = condition, y = expression, fill = condition))
     y = "Expression level"
   ) +
   theme_classic()
+ggsave("Output/boxplot_of_condition.png")
+
 
 # NOTE expression levels seems much higher in caner patients based on this
 # boxplot and also they seems to be older patients too, based on this boxplot
@@ -55,13 +57,15 @@ ggplot(data = merged_data, aes(x = condition, y = expression, fill = condition))
 # age and higher expression level
 
 # To further investigate that claim we will make a scatter plot
-
-with(merged_data, {
+png("Output/scatter_plot.png")
+scatter_plot <- with(merged_data, {
   plot(age ~ expression)
   linear_model <- lm(expression ~ age)
   abline(linear_model)
 })
+dev.off()
 # NOTE Again the expression levels looks higher in older patients
+
 
 ## Differential Expression Analysis
 
@@ -79,3 +83,9 @@ t_test$p_adjusted <- p.adjust(t_test$unadjusted_p_values, method = "bonferroni")
 significant_p_values <- t_test %>% filter(p_adjusted <= 0.05)
 
 significant_p_values # NOTE gene 001 and 004 and 005 are significant
+significant_genes <- significant_p_values$gene_id
+selected_genes <- merged_data %>% filter(gene_id %in% significant_genes) #nolint
+
+significant_gene_boxplot <- ggplot(data = selected_genes, aes(x = condition, y = expression, color = condition)) +
+  geom_boxplot() + facet_wrap(gene_id~.)
+ggsave("Output/significant_gene_boxplot.png")
